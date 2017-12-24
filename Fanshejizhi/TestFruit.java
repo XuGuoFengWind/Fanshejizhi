7fecd6a4ea2baa65223046006943dc02cf021ea0
Like Wind <34294454+XuGuoFengWind@users.noreply.github.com>
2017-12-24 22:33:45 +0800

    反射机制

---
+++ Fanshejizhi/TestFruit.java
@@ -0,0 +1,37 @@
+interface Person{
+	public void eat();
+}
+class Apple implements Person{
+	@Override
+	public void eat() {
+		System.out.println("吃苹果");
+	}
+}
+class Banana implements Person{
+	@Override
+	public void eat(){
+		System.out.println("吃香蕉");
+	}
+}
+  //工厂类
+class Factory{
+	public static Person getInstance(String className) {
+		switch(className) {
+		case"苹果":
+			return new Apple();
+		case"香蕉":
+			return new Banana();
+		default:
+			return null;	
+		}
+	}
+}
+public class TestFruit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p=Factory.getInstance("苹果");
+		p.eat();
+	}
+
+}
